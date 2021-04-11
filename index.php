f164782fd631d127f07337691c6652a7681732db
AlePana-ua <apa84@alu.ua.es>
2021-04-11 15:41:39 +0200

    Mejoras de interfaz

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php include 'header.php'; ?>
 <!-- Inicio Buscador -->
 <div class="h-75 d-inline-block search-div">
-  <div class="row align-items-center justify-content-around search-box">
+  <div class="container-fluid">
     <form action="listaViviendas.php" method="POST">
-      <div class="form-row align-items-center">
+      <div class="row align-items-center search-box">
         <div class="col">
-          <label for="Name">Ciudad</label>
-            <select class="custom-select" name="destino">
-              <option value="">Buscar ciudad ...</option>
-              <?php require_once 'utils.php';
-                echo get_list_of_cities();
-              ?> 
-            </select>
+          <label id="lista-ciudades" for="Name">Ciudad</label>
+          <select class="custom-select" id="city" name="destino">
+            <option value="">Buscar ciudad ...</option>
+            <?php require_once 'utils.php';
+              echo get_list_of_cities();
+            ?> 
+          </select>
         </div>
+        <div id="verticle-line"></div>
         <div class="col">
-          <label for="Name">Llegada</label>
-          <input id="datepicker" width="200" placeholder="¿Cuándo?" name="fecha-llegada"/>
+          <label id="llegada" for="Name">Llegada</label>
+          <input id="datepicker" placeholder="¿Cuándo?" name="fecha-llegada"/>
         </div>
+        <div id="verticle-line"></div>
         <div class="col">
-          <label for="Name">Salida</label>
-          <input id="datepicker1" width="200" placeholder="¿Cuándo?" name="fecha-salida"/>
+          <label id="salida" for="Name">Salida</label>
+          <input id="datepicker1"  placeholder="¿Cuándo?" name="fecha-salida"/>
         </div>
         <div class="col">
           <button type="submit" class="btn btn_custom">Buscar</button>
@@ -40,18 +42,41 @@
 
 <!-- Inicio lista de ciudades -->
 <div class="container-fluid">
-  <h1 class="card-title">¿Tú próximo destino?</h2>
+  <br>
+  <h1 id="cities-title" class="card-title">¿Tú próximo destino?</h2>
+  <br>
   <div class="row">
     <?php require_once 'utils.php';
       // Esto podría ser una llamada que obtenga las ciudades
-      $cities_array = array("Alicante", "Valencia", "Madrid");
+      $cities_array = array("Alicante", "Valencia", "Madrid", "1", "2", "3");
+      $num_city = 0;
       foreach ($cities_array as $city) {
-        echo get_cities_cards($city);
+        echo get_cities_cards($city, $num_city);
+        $num_city += 1;
       }
     ?>
   </div>
+  <br>
 </div>
 <!-- Fin lista de ciudades -->
+
+<!-- Inicio lista de zonas -->
+<div id="zone-type-list" class="container-fluid">
+  <br>
+  <h2 id="zone-type" class="zone-type">Eres más de ...</h2>
+  <br>
+  <div class="row">
+    <?php require_once 'utils.php';
+      // Esto podría ser una llamada que obtenga las ciudades
+      $house_type_array = array("playa", "montana", "ciudad");
+      foreach ($house_type_array as $house_type) {
+        echo get_house_type_cards($house_type);
+      }
+    ?>
+  </div>
+  <br>
+</div>
+<!-- Fin lista de zonas -->
 
 
 <?php include 'footer.php'; ?>
--- listaViviendas.php
+++ listaViviendas.php
@@ -2,37 +2,36 @@
 <?php include_once "conexionBD.inc"; ?>
 
 <div class="row h-auto">
-    <div class="col-sm-10">
-        <?php
-        $_SESSION['destino'] = $_POST['destino'];
-        $_SESSION['fecha-llegada'] = $_POST['fecha-llegada'];
-        $_SESSION['fecha-salida'] = $_POST['fecha-salida'];
+  <div class="col-sm-10">
+    <?php
+			require_once 'utils.php';
 
-        echo '<h2>Viviendas en '.$_SESSION['destino'].'</h2>';
-        echo '<h4>Llegada: '.$_SESSION['fecha-llegada'].'  Salida: '.$_SESSION['fecha-salida'].'</h4>';
+			$_SESSION['destino'] = $_POST['destino'];
+			$_SESSION['fecha-llegada'] = $_POST['fecha-llegada'];
+			$_SESSION['fecha-salida'] = $_POST['fecha-salida'];
 
-        require_once 'utils.php';
+			echo '<h3>Mostrando viviendas en '.$_SESSION['destino'].'</h3>';
+			echo '<h5>Llegada: '.$_SESSION['fecha-llegada'].'  Salida: '.$_SESSION['fecha-salida'].'</h5>';
 
-        $totalDias = 10;
-        
-        $query = $link->query("SELECT idVivienda, foto, descripcion from Anuncio");
-        if ($query->num_rows == 0)
-        {
-            echo "Lo sentimos no hay casas.";
-        } else 
-        {
-            if($row = mysqli_fetch_array($query))
-            {
-                for($i=0; $i <= 3; $i++) {
-                    echo get_house_cards(1, 1, 1, 1);
-                }
-            }else {
-                echo "<h3> Error al mostrar las casa, intente de nuevo mas tarde</h3>";
-            }
-        }
-        ?>
-    </div>
+			//para pruebas sin datos
+			$totalDias = 10;
+			
+			$query = $link->query("SELECT idVivienda, foto, descripcion from Anuncio");
+			if ($query->num_rows == 0) {
+					echo house_not_found();
+			} else {
+					if($row = mysqli_fetch_array($query)) {
+							for($i=0; $i <= 3; $i++) {
+									echo get_house_cards(1, 1, 1, 1);
+							}
+					}else {
+							echo house_not_found();//"<h3> Error al mostrar las casa, intente de nuevo mas tarde</h3>";
+					}
+			}
+    ?>
+  </div>
 </div>
+
 <?php include_once "desconexionBD.inc"; ?>
 <?php include 'footer.php'; ?>
 
