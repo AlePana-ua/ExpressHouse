26a7ff0107bf5a8d75a499dcfc5c61c982e5f597
AlePana-ua <apa84@alu.ua.es>
2021-04-24 10:18:13 +0200

    mejoras varias

--- listaViviendas.php
+++ listaViviendas.php
@@ -1,38 +1,44 @@
 <?php include 'header.php'; ?>
-<?php include_once "conexionBD.inc"; ?>
+<?php //include_once "conexionBD.inc"; ?>
 
 <div class="row h-auto">
   <div class="col-sm-12">
-    <?php
-			require_once 'utils.php';
+    <?php require_once 'utils.php';
+		//Variablees que se pasan desde el buscador de la página inicial.
+		$destino = $_POST['destino'];
+		$fecha_llegada = $_POST['fecha-llegada'];
+		$fecha_salida = $_POST['fecha-salida'];
 
-			$_SESSION['destino'] = $_POST['destino'];
-			$_SESSION['fecha-llegada'] = $_POST['fecha-llegada'];
-			$_SESSION['fecha-salida'] = $_POST['fecha-salida'];
+		echo '<h3>Mostrando viviendas en '.$destino.'</h3>';
+		echo '<h5>Llegada: '.$fecha_llegada.'&nbsp Salida: '.$fecha_salida.'</h5>';
 
-			echo '<h3>Mostrando viviendas en '.$_SESSION['destino'].'</h3>';
-			echo '<h5>Llegada: '.$_SESSION['fecha-llegada'].'  Salida: '.$_SESSION['fecha-salida'].'</h5>';
+		//para pruebas sin datos
+		$totalDias = 10;
+		// Mostramos las casas que cumplen con los criterios de busqueda
+		/*$query = $link->query("SELECT idVivienda, foto, descripcion from Anuncio");
+		if ($query->num_rows == 0) {
+				echo house_not_found();
+		} else {
+				if($row = mysqli_fetch_array($query)) {
+						for($i=0; $i <= 3; $i++) {
+								echo get_house_cards(1, 1, 1, 1);
+						}
+				}else {
+						echo house_not_found();//"<h3> Error al mostrar las casa, intente de nuevo mas tarde</h3>";
+				}
+		}*/
 
-			//para pruebas sin datos
-			$totalDias = 10;
-			
-			$query = $link->query("SELECT idVivienda, foto, descripcion from Anuncio");
-			if ($query->num_rows == 0) {
-					echo house_not_found();
-			} else {
-					if($row = mysqli_fetch_array($query)) {
-							for($i=0; $i <= 3; $i++) {
-									echo get_house_cards(1, 1, 1, 1);
-							}
-					}else {
-							echo house_not_found();//"<h3> Error al mostrar las casa, intente de nuevo mas tarde</h3>";
-					}
-			}
+		/* En caso que la conexión falle, comentar el código anterior
+		* y utilizar el siguiente.
+		*/
+		for($i=0; $i <= 2; $i++) {
+			echo get_house_cards(0, 1, 'Una casa bonita', 25, $_SESSION['destino']);
+		}
     ?>
   </div>
 </div>
 
-<?php include_once "desconexionBD.inc"; ?>
+<?php //include_once "desconexionBD.inc"; ?>
 <?php include 'footer.php'; ?>
 
 
