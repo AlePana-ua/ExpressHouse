7fbb924867b75348ba7da6d6699f898f66a0e361
AlePana-ua <apa84@alu.ua.es>
2021-06-10 11:09:42 +0200

    Script para insertar datos

---
+++ crearReseña.php
@@ -0,0 +1,97 @@
+<?php include 'header.php';
+    include "conexionBD.inc"; ?>
+ <!-- Masthead -->
+<div class="row h-100">
+  <div class="col-sm-12 my-auto">
+    <div class="card card-block w-50 mx-auto" >
+      <div class="card-body" >
+          <h2 class="title" style="text-align: center;">Crear Reseña</h2>
+          <hr>
+          <form method="POST" name="formularioCrearReseña" action="crearReseña_Post.php">
+              <div class="form-row">
+                  <div class="form-group col-md-6">
+
+                    <!-- enviar id_vivienda al pulsar boton crearReseña -->
+
+                      <label class="label">ID</label>
+                      <?php  
+                        if(isset($_POST['id_vivienda'])){
+                            $id_vivienda=$_POST['id_vivienda'];
+                            $fecha=$_POST['fecha'];
+                            $id_usuario=$_POST['id_usuario'];
+                            echo " casa nº " .$id_vivienda. "<br>";       
+                        }
+                        else 
+                            echo "NO ID, acceder desde vivienda"
+                       ?> 
+                     <!-- <input class="form-control" type="text" name="nombre" vale="ejemplo"> -->
+                    
+                    <input type="hidden" name="id_vivienda" value=<?php $id_vivienda ?> > 
+                    <input type="hidden" name="fecha" value=<?php $fecha ?> >
+                    <input type="hidden" name="id_usuario" value=<?php $id_usuario ?> >
+                  </div>
+              </div>
+              <div class="form-row">
+                  <div class="form-group col-md-12">
+                      <label class="label">Descripcion</label>
+                      <!--<textarea name="mensaje" rows="10" cols="40">Escribe aquí tus comentarios</textarea> -->
+
+                       <input class="form-control" type="text" name="descripcion" size=800> 
+                  </div>
+              </div>
+              <hr>
+                   
+            <table class="table table-borderless">
+                <tbody>
+                    <tr>
+                        
+                        <td>Puntuacion</td>
+                        <td>
+                            <form>
+                                <p class="clasificacion">
+                                    <input id="radio1" type="radio" name="estrellas" value="5"><!--
+                                    --><label for="radio1">★</label><!--
+                                    --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+                                    --><label for="radio2">★</label><!--
+                                    --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+                                    --><label for="radio3">★</label><!--
+                                    --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+                                    --><label for="radio4">★</label><!--
+                                    --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+                                    --><label for="radio5">★</label>
+                                </p>
+                            </form>
+                        </td>
+                        
+                        
+                         
+                    </tr>
+                  
+                    
+                </tbody>
+               
+            </table>
+
+            
+
+
+            <div class="form-row" style="text-align: center;">
+                <div class="col text-center" >
+                    <button class="btn btn-primary" type="submit" style="text-align: center;">Enviar</button>
+                </div>
+            </div>
+          </form>
+      </div>
+    </div>
+   
+  </div>
+ 
+</div>
+
+<?php include 'footer.php'; ?>
+
+
+
+<!-- https://es.stackoverflow.com/questions/126046/valoracion-estrellas-1-5-dise%C3%B1o-estrellas-php-html 
+http://w3.unpocodetodo.info/css3/estrellas.php 
+-->
---
+++ listarReseña.php
@@ -0,0 +1,104 @@
+<?php 
+    session_start();
+    include "conexionBD.inc";
+    //include "seguridadAdmin.php";
+    
+    
+    // Titulo que se muestra en la pestaña del navegador.
+    $pageTitle = 'Lista Reseñas';
+    include 'header.php'; 
+    
+    // Comprobamos en que página de la lista estamos. 
+    if(isset($_GET['page']) && isset($_POST['id_usuario']) ) {
+        $page = $_GET['page'];
+        $id_usuario = $_POST['id_usuario'];
+    }else {
+        echo "error usuario";
+        $page = 1;
+    }
+    // Cantidad de usuarios por página.
+    $resultados_por_pagina = 20;
+    // Número de página donde comenzar la nueva query 
+    $start_from = ($page-1) * $resultados_por_pagina;
+?>
+<br>
+
+<div class="h-auto container-fluid">
+    <div class="container">
+        <!-- Boton para volver a inicio-->
+        <div class="row d-flex justify-content-left"> 
+            <form method="POST" action="index.php">
+                <button class="btn btn-volver" type="submit">Volver a inicio</button>
+            </form>
+        </div>
+        <!-- Lista de reseñas extraidas de la base de datos -->
+        <div class="row d-flex justify-content-center">
+            <h2>Lista de Reseñas</h2>
+            <?php
+                $query_resenyas= "SELECT * FROM resenya WHERE id_usuario='$id_usuario' ORDER BY id_resenya ASC LIMIT $start_from, $resultados_por_pagina;";
+                if($query = $link->query($query_resenyas)){
+            ?>
+            <table class="table table-sm table-striped">
+                <thead class="table-light">
+                    <tr>
+                        <th>Usuario</th>
+                        <th>Vivienda</th>
+                        <th>Fecha</th>
+                        <th>Putuación</th>
+                        <th>Descripción</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    while($row = mysqli_fetch_array($query)) {
+                        $id_resenya = $row['id_reseña'];
+                        $id_usuario = $row['id_usuario'];
+                        $id_vivienda = $row['id_vivienda'];
+                        $fecha = $row['fecha'];
+                        $puntuacion = $row['puntuacion'];
+                        $descripcion = $row['descripcion'];
+                ?>
+                    <tr>
+                        <td><?php echo utf8_encode($id_usuario);?></td>
+                        <td><?php echo utf8_encode($id_vivienda);?></td>
+                        <td><?php echo utf8_encode($fecha);?></td>
+                        <td><?php echo utf8_encode($puntuacion);?></td>
+                        <td><?php echo utf8_encode($descripcion);?></td>
+                    </tr>
+                <?php           
+                    }
+                }else { //Fin if($query...)
+                    // En caso de un error en la query lo mostramos.
+                    echo $link->error;
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <!-- Lista de páginas con reseñas -->
+        <div class="row d-flex justify-content-center">
+            <?php
+                $query_paginas = "SELECT COUNT(id_resenya) AS total FROM resenya";
+                if($query2 = $link->query($query_paginas)){
+                    while($row = mysqli_fetch_array($query2)) {
+                        $total_pages = ceil( $row['total']/ $resultados_por_pagina);
+                    }
+                    for($i=1; $i<=$total_pages ;$i++) {
+                        echo "<a href=\"lista_usuarios.php?page=$i\">$i&nbsp</a>";
+                        
+                    }
+                }else {
+                    echo $link->error;
+                }
+            ?>
+        </div>
+    </div>
+</div>
+
+
+<?php 
+    include "desconexionBD.inc";
+    include 'footer.php'; 
+?>
